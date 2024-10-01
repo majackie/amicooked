cf1e0f589c9f0138c4bf7c46836f7399bd715f86
Tealiosv2 <97336208+Tealiosv2@users.noreply.github.com>
2024-10-01 11:56:01 -0700

    test files

---
+++ src/Test.js
@@ -0,0 +1,15 @@
+
+import logo from './logo.svg';
+import './App.css';
+
+function Test() {
+  return (
+    <div className="App">
+    <p>
+        hello world
+    </p>
+    </div>
+  );
+}
+
+export default Test;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Test from './Test';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+   <Routes>
+   <Route path='/' element={<App />}></Route> 
+   <Route path='/test' element=
+    {<Test />}>
+</Route> 
+    </Routes> 
+    </BrowserRouter>
+
   </React.StrictMode>
 );
 
