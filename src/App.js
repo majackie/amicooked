3975a88392556ab531ed7abf55dcc923cd89af3d
Billy Nguyen <bquynhnguyen209@gmail.com>
2024-10-06 23:57:32 -0700

    Added SafetyLessons page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Routes, Route } from "react-router-dom";
 // Pages
 import Home from './pages/Home';
 import UserDashboard from './pages/UserDashboard'
+import SafetyLessons from "./pages/SafetyLessons";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/home" element={<Home />} /> 
         {/* Add pages here */}
         <Route path="/user-dashboard" element={<UserDashboard />} />
+        <Route path="/user-dashboard/safety-lessons" element={<SafetyLessons />} />
       </Routes>
     </>
   );
