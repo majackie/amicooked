ba134eaf9bbed8108e0ba65f5073d5b882957c6a
pcammayo <pcammayo03@gmail.com>
2024-10-08 01:59:12 -0700

    Added UI to CategoryCard.

--- src/components/TopCategories/CategoryCard.js
+++ src/components/TopCategories/CategoryCard.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
-function CategoryCard() {
-    return <div>LatestPosts</div>;
+function CategoryCard({ categoryName }) {
+  return (
+    <div style={styles.card}>
+      {categoryName}
+    </div>
+  );
+}
+
+const styles = {
+    card: { 
+        border: '0.5vh solid #3b82f6',
+        borderRadius: '5px',
+        padding: '1.5vh',
+        margin: '1.5vh' 
+    }
 }
 
 export default CategoryCard;
