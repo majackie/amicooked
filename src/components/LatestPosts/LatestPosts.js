18a62b32066ae57f070ab858ca45390dab330384
pcammayo <pcammayo03@gmail.com>
2024-10-08 02:20:43 -0700

    Added UI to LatestPosts.

--- src/components/LatestPosts/LatestPosts.js
+++ src/components/LatestPosts/LatestPosts.js
@@ -1,7 +1,30 @@
+// src/components/LatestPosts/LatestPosts.js
 import React from 'react';
+import PostCard from './PostCard';
 
 function LatestPosts() {
-    return <div>LatestPosts</div>;
+  return (
+    <section style={styles.section}>
+      <h2>Latest Posts</h2>
+      <div style={styles.filter}>
+        <select>
+          <option>All Categories</option>
+        </select>
+      </div>
+      {[1, 2, 3, 4, 5].map((_, idx) => (
+        <PostCard key={idx} postTitle="lorem ipsum" />
+      ))}
+    </section>
+  );
+}
+
+const styles = {
+    section: { 
+        padding: '2vh' 
+    },
+    filter: {
+        marginBottom: '2vh' 
+    }
 }
 
 export default LatestPosts;
