74f7274ff31cc31920e9305f7c7bc89d56c55155
pcammayo <pcammayo03@gmail.com>
2024-10-08 02:21:13 -0700

    Added UI to BackButton.

--- src/components/BackButton.js
+++ src/components/BackButton.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
 function BackButton() {
-    return <div>BackButton</div>;
+  return (
+    <div style={styles.wrapper}>
+      <button style={styles.button}>Back to Dashboard</button>
+    </div>
+  );
 }
 
-export default BackButton
+const styles = {
+    wrapper: {
+        textAlign: 'center',
+        margin: '20px'
+    },
+    button: {
+        padding: '10px 20px',
+        borderRadius: '5px',
+        backgroundColor: '#d1d5db'
+    }
+}
+
+export default BackButton;
