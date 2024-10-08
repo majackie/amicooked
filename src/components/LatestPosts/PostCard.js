3dfc2da147729e688f98dc0d6e8e3a166e90adad
pcammayo <pcammayo03@gmail.com>
2024-10-08 02:20:21 -0700

    Added UI to PostCard.

--- src/components/LatestPosts/PostCard.js
+++ src/components/LatestPosts/PostCard.js
@@ -1,7 +1,21 @@
+// src/components/LatestPosts/PostCard.js
 import React from 'react';
 
-function PostCard() {
-    return <div>PostCard</div>;
+function PostCard({ postTitle }) {
+  return (
+    <div style={styles.postCard}>
+      {postTitle}
+    </div>
+  );
+}
+
+const styles = {
+    postCard: {
+        border: '1px solid #3b82f6',
+        borderRadius: '5px',
+        padding: '20px',
+        margin: '10px'
+    }
 }
 
 export default PostCard;
