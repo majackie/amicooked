2a47ffc3c052bc3d9f9184be8f01e5a7f388e634
Tealiosv2 <97336208+Tealiosv2@users.noreply.github.com>
2024-11-24 01:32:07 -0800

    package fixes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,17 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import Test from "./Test";
 import Admin from "./Admin";
 import Login from "./Login";
 import Protected from "./Protected";
 import Signup from "./Signup";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import './index.css';
-import reportWebVitals from './reportWebVitals';
 
-import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -21,7 +17,6 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}></Route>
-        <Route path="/test" element={<Test />}></Route>
         <Route path="/admin" element={<Admin/>}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/signup" element={<Signup />}></Route>
