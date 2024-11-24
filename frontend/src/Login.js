de4e2fe8b6e94472634eaa7776cffc0a90c488e0
Tealiosv2 <97336208+Tealiosv2@users.noreply.github.com>
2024-11-24 14:59:43 -0800

    styled login

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,46 +1,47 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom"; // Import useNavigate
+import { useNavigate } from "react-router-dom";
+import "./style/Login.css";
 
 const Login = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-    const navigate = useNavigate(); // Initialize navigate
+    const navigate = useNavigate();
 
     const handleLogin = async () => {
-        console.log("handleLogin")
         try {
             const response = await axios.post(`http://127.0.0.1:5000/api/login`, { 
                 username: username, 
                 password: password,
             });
             localStorage.setItem("token", response.data.access_token);
-            localStorage.setItem("id", response.data.userid)
-            console.log("access token "+localStorage.getItem("token"))
+            localStorage.setItem("id", response.data.userid);
             alert("Login successful!");
 
-            // Redirect to user-dashboard after successful login
             navigate("/user-dashboard");
         } catch (error) {
-            alert("Login failed: " + error.response?.data?.msg || "Something went wrong");
+            alert("Login failed: " + (error.response?.data?.msg || "Something went wrong"));
         }
     };
 
     return (
-        <div>
-            <input
-                type="text"
-                placeholder="Username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-            />
-            <input
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-            />
-            <button onClick={handleLogin}>Login</button>
+        <div className="LoginPage">
+            <div className="LoginContainer">
+                <h1>Login</h1>
+                <input
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button onClick={handleLogin}>Login</button>
+            </div>
         </div>
     );
 };
