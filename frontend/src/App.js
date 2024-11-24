588bf5b1c5902747e1777b1f0a1a0103d740d5da
Billy Nguyen <bquynhnguyen209@gmail.com>
2024-11-24 02:00:52 -0800

    Changed localhost to 127.0.0.1; Fixed App.js routing issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,10 @@
 import {Routes, Route } from "react-router-dom";
 
 // Pages
+import Admin from "./Admin.js";
+import Login from "./Login";
+import Logout from "./Logout";
+import Signup from "./Signup";
 import UserDashboard from './pages/UserDashboard'
 import SafetyTools from "./pages/SafetyTools";
 import PrivacyChecker from "./pages/PrivacyChecker";
@@ -11,25 +15,33 @@ import Phishing from "./pages/interactive/Phishing";
 import TermsNConditions from "./pages/interactive/TermsNConditions"
 import PhishingUrl from "./pages/interactive/PhishingUrl.js";
 import Tips from "./pages/Tips";
+import ProtectedRoute from "./shared/ProtectedRoute.js";
 
 function App() {
   return (
     <>
       <Routes>
         {/* Landing Page */}
-        <Route path="/" element={<UserDashboard />} />
+        {/* <Route path="/" element={<Home />} /> */}
         {/* <Route path="/home" element={<Home />} />  */}
+        <Route path="/" element={<Login />}></Route>
+        <Route path="/admin" element={<Admin/>}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/logout" element={<Logout />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+        
         {/* Add pages here */}
         <Route path="/user-dashboard" element={<UserDashboard />} />
         <Route path="/user-dashboard/safety-tools" element={<SafetyTools />} />
         <Route path="/user-dashboard/safety-tools/privacy-checker" element={<PrivacyChecker />}/>
         <Route path="/user-dashboard/safety-tools/sagwa" element={<Sagwa />} />
-        {/* Lesson pages */}
-        <Route path="/user-dashboard/safety-tools/lessons-home" element={<LessonsHome />} />
-        <Route path="/user-dashboard/safety-tools/lesson/:topicId" element={<Lesson />} />
-        <Route path="/user-dashboard/safety-tools/lesson/1/interactive" element={<Phishing />} />
-        <Route path="/user-dashboard/safety-tools/lesson/3/interactive" element={<TermsNConditions />} />
-        <Route path="/dangerous-phishing-url" element={<PhishingUrl/>} />
+        {/* Protected Lesson pages */}
+        <Route path="/user-dashboard/safety-tools/lessons-home" element={<ProtectedRoute><LessonsHome /></ProtectedRoute>} />
+        <Route path="/user-dashboard/safety-tools/lesson/:topicId" element={<ProtectedRoute><Lesson /></ProtectedRoute>} />
+        <Route path="/user-dashboard/safety-tools/lesson/1/interactive" element={<ProtectedRoute><Phishing /></ProtectedRoute>} />
+        <Route path="/user-dashboard/safety-tools/lesson/3/interactive" element={<ProtectedRoute><TermsNConditions /></ProtectedRoute>}/>
+        <Route path="/dangerous-phishing-url" element={<ProtectedRoute><PhishingUrl/></ProtectedRoute>} />
+
         <Route path="/user-dashboard/safety-tools/tips/:topicId" element={<Tips />} />
       </Routes>
     </>
