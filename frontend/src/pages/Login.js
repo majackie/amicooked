ade4fb7c79a6df75453c4afb31c844a5100a8ebb
Tealiosv2 <97336208+Tealiosv2@users.noreply.github.com>
2024-11-24 20:46:36 -0800

    navbar

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -2,48 +2,54 @@ import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import "../style/Login.css";
+import Navbar from "../shared/Navbar";
 
 const Login = () => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-    const handleLogin = async () => {
-        try {
-            const response = await axios.post(`http://127.0.0.1:5000/api/login`, { 
-                username: username, 
-                password: password,
-            });
-            localStorage.setItem("token", response.data.access_token);
-            localStorage.setItem("id", response.data.userid);
-            alert("Login successful!");
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post(`http://127.0.0.1:5000/api/login`, {
+        username: username,
+        password: password,
+      });
+      localStorage.setItem("token", response.data.access_token);
+      localStorage.setItem("id", response.data.userid);
+      alert("Login successful!");
 
-            navigate("/user-dashboard");
-        } catch (error) {
-            alert("Login failed: " + (error.response?.data?.msg || "Something went wrong"));
-        }
-    };
+      navigate("/user-dashboard");
+    } catch (error) {
+      alert(
+        "Login failed: " + (error.response?.data?.msg || "Something went wrong")
+      );
+    }
+  };
 
-    return (
-        <div className="LoginPage">
-            <div className="LoginContainer">
-                <h1>Login</h1>
-                <input
-                    type="text"
-                    placeholder="Username"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                />
-                <input
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                <button onClick={handleLogin}>Login</button>
-            </div>
+  return (
+    <div>
+      <Navbar type={"default"} />
+      <div className="LoginPage">
+        <div className="LoginContainer">
+          <h1>Login</h1>
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button onClick={handleLogin}>Login</button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
